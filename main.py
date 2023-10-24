dfce7087f6a3d7d872d7f7575cc6d7a30d938a78
marcosaccomanno <marcosaccomanno@protonmail.com>
2023-10-24 20:00:36 -0300

    Added colorama and 'qr_files' folder

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import segno
-
-# user input to generate qr code
-data_input = input('Insert text or link to generate QR code: ')
-
-# user input for generated file name, appends the file type
-qr_img_name = input('File name (if a file with that name already exists it will be replaced!): ') + '.svg'
+from colorama import Fore
 
 def generateQr(data, name):
     '''
@@ -14,10 +9,20 @@ def generateQr(data, name):
         '''
     data_to_qr = segno.make(data, error='q')
     data_to_qr.save(name, scale=10, dark='black', light=None)
-    print(f'QR code generated successfully and saved as "{name}"')
+    print(Fore.GREEN + f'QR code generated successfully and saved as "{name}"')
 
 if __name__ == "__main__":
-    generateQr(data_input, qr_img_name)
+    while True:
+        print(Fore.YELLOW + '-------------QR SVG GENERATOR (Press CTRL+C to exit)-------------')
+        # user input to generate qr code
+        data_input = input(Fore.RESET + 'Insert text or link to generate QR code: ')
+
+        # user input for generated file name, appends the file type
+        qr_img_name = "qr_files/" + input('File name ' + Fore.RED + '(if a file with that name already exists it will be replaced!): ' + Fore.RESET) + '.svg'
+        
+        generateQr(data_input, qr_img_name)
+        
+
 
 
 
