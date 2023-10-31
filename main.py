77a3d49f45cbe7dbc13ae33da8be53bf4d0b413a
marcosaccomanno <95966510+marcosaccomanno@users.noreply.github.com>
2023-10-31 16:26:49 -0300

    Corrected file naming

--- main.py
+++ main.py
@@ -6,31 +6,30 @@ def generateQr(data):
         This function takes the data entered by the user, 
         generates a QR code and saves it as a SVG image.
         '''
-    # qr code is generated
+    # generate qr
     data_to_qr = segno.make(data, error='q')
-    # output image name is defined
-    file_name = 'qr_files/' + data + '.svg'
-    # output image is saved as SVG
-    data_to_qr.save(file_name, scale=10, dark='black', light=None)
-    # success message is displayed
+    # define image name and path
+    file_name = data.replace('/', '.')
+    file_path = 'qr_files/' + file_name + '.svg'
+    # save qr image
+    data_to_qr.save(file_path, scale=10, dark='black', light=None)
+    # print success message
     print(Fore.GREEN + f'QR code generated successfully and saved in the "qr_files" folder.')
 
 if __name__ == "__main__":
+    # program starts
     program_active = True
     while program_active == True:
         print(Fore.YELLOW + '-------------QR SVG GENERATOR-------------' +  Fore.RESET)
-        # user enters text or link to generate qr code
+        # user input to generate qr code
         data_input = input('Insert text or link to generate QR code: ')
-        # function for generating qr code is called
+        # generate qr
         generateQr(data_input)
-        # ask if the user wants to generate another qr code
+        # ask if the user wants to generate another qr
         program_continue = input(Fore.YELLOW + 'Generate another QR? (y/n): ' + Fore.RESET)
         if program_continue != 'y':
             print(Fore.YELLOW + '------------------------------------------' +  Fore.RESET)
             # program ends
             program_active = False
-
-
-
 
 
