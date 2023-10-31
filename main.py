ae7469ad7b4e0a7a11ae2a1257ec78bdc281a4d1
marcosaccomanno <95966510+marcosaccomanno@users.noreply.github.com>
2023-10-31 16:01:10 -0300

    Improved comments

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ def generateQr(data):
         This function takes the data entered by the user, 
         generates a QR code and saves it as a SVG image.
         '''
-    # generate qr
+    # qr code is generated
     data_to_qr = segno.make(data, error='q')
-    # define image name
+    # output image name is defined
     file_name = 'qr_files/' + data + '.svg'
-    # save qr image
+    # output image is saved as SVG
     data_to_qr.save(file_name, scale=10, dark='black', light=None)
-    # print success message
+    # success message is displayed
     print(Fore.GREEN + f'QR code generated successfully and saved in the "qr_files" folder.')
 
 if __name__ == "__main__":
-    # program starts
     program_active = True
     while program_active == True:
         print(Fore.YELLOW + '-------------QR SVG GENERATOR-------------' +  Fore.RESET)
-        # user input to generate qr code
+        # user enters text or link to generate qr code
         data_input = input('Insert text or link to generate QR code: ')
-        # generate qr
+        # function for generating qr code is called
         generateQr(data_input)
-        # ask if the user wants to generate another qr
+        # ask if the user wants to generate another qr code
         program_continue = input(Fore.YELLOW + 'Generate another QR? (y/n): ' + Fore.RESET)
         if program_continue != 'y':
             print(Fore.YELLOW + '------------------------------------------' +  Fore.RESET)
